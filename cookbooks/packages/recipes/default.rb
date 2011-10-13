a3eaf072ecdd163cf9fd4acad7af1ff8293752d4
Avrohom Katz <akatz@engineyard.com>
2011-10-13 13:47:19 -0400

    root bug

--- cookbooks/packages/recipes/default.rb
+++ cookbooks/packages/recipes/default.rb
@@ -4,8 +4,8 @@
 #
 
 remote_file "/engineyard/portage/engineyard/www-servers/nginx/nginx-0.8.55-r2.ebuild"  do 
-  owner root
-  group root
+  owner "root"
+  group "root"
   mode 0644 
   source "nginx-0.8.55.r2.ebuild"
 end 
@@ -16,6 +16,6 @@ execute "emerge --sync"
 
 execute "upgrade nginx" do
   command "emerge -1n nginx"
-  user root
+  user "root"
   not_if "emerge --search nginx |grep installed | awk '{print $4}'" == "emerge --search nginx |grep available | awk '{print $4}'"
 end
